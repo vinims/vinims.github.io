7e04c6f8bd39589735a4b064e1798c1cb1cd32f0
vinims <yvymaia@hotmail.com>
2018-07-08 12:11:15 -0600

    fixing http

--- assignments/lesson-9/scripts/weatherapi.js
+++ assignments/lesson-9/scripts/weatherapi.js
@@ -1,6 +1,6 @@
 var weatherRequest = new XMLHttpRequest();
 
-var apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=4156210&APPID=6a274f6ccb2c7781e5011ec433a0f665&units=Imperial";
+var apiURL = "//api.openweathermap.org/data/2.5/forecast?id=4156210&APPID=6a274f6ccb2c7781e5011ec433a0f665&units=Imperial";
 
 weatherRequest.open('GET', apiURL, true);
 weatherRequest.send();   
