42f73e06d09fe9efbab43f70fc6dc7d6d97e1c3c
vinims <yvymaia@hotmail.com>
2018-07-08 12:39:13 -0600

    fixing

---
+++ assignments/weathersite/scripts/wayfinder.js
@@ -0,0 +1,16 @@
+    
+        var urlString, urlArray, pageHREF, menu, i, currentURL;
+        urlString = document.location.href;
+        urlArray = urlString.split('/');
+        pageHREF = urlArray[urlArray.length - 1];
+
+        if (pageHREF !== "") {
+            menu = document.querySelectorAll('ul#mainmenu li a');
+            for (i = 0; i < menu.length; i++) {
+                currentURL = (menu[i].getAttribute('href'));
+                menu[i].parentNode.className = '';
+                if (currentURL === pageHREF) {
+                    menu[i].parentNode.className = 'active';
+                }
+            }
+        }
