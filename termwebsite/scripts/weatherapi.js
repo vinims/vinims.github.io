b39b16587e7c48d74663a34e414eaf23c2b6f17d
vinims <yvymaia@hotmail.com>
2018-07-28 12:52:27 -0600

    Termwebsite

--- termwebsite/scripts/weatherapi.js
+++ termwebsite/scripts/weatherapi.js
@@ -1,17 +1,6 @@
 var weatherRequest = new XMLHttpRequest();
 
-var apiURL = "//api.openweathermap.org/data/2.5/forecast?id=4156210&APPID=6a274f6ccb2c7781e5011ec433a0f665&units=Imperial";
 
-weatherRequest.open('GET', apiURL, true);
-weatherRequest.send();   
-
-weatherRequest.onload =  function () {
-  var weatherData = JSON.parse(weatherRequest.responseText);
-  console.log(weatherData);
-    
-    var temp = weatherData.list[0].main.temp;
-    document.getElementById('current-temp').innerHTML = temp;
-};
 
 
 
@@ -32,8 +21,8 @@ function bozemanweather(){
             var apiIcon = weatherData.list[0].weather[0].icon;
             var iconURL = '//openweathermap.org/img/w/' + apiIcon + '.png';
             
-            document.getElementById('weathercondition').src = iconURL;
-            document.getElementById('weathertemp').innerHTML = apiTempC + ' °F';
+            document.getElementById('weathericon').src = iconURL;
+            document.getElementById('weathertempFah').innerHTML = apiTempC + ' °F';
             
         };
 }
